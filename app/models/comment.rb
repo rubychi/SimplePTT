42eb6205682628284ce295a704faac996dfd0357
Ruby Chi <c90206@gmail.com>
2019-09-15 03:21:06 +0800

    fix(models/comment): fix unable to parse sql syntax due to switch from sqlite to postgresql

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,8 +3,8 @@ class Comment < ApplicationRecord
   belongs_to :post
   validates :content, presence: true
 
-  scope :num_of_likes, -> (postId) { select('count(*)').where('post_id=? AND like > 0', postId) }
-  scope :num_of_dislikes, -> (postId) { select('count(*)').where('post_id=? AND dislike > 0', postId) }
-  scope :liked?, -> (postId, userId) { select('count(*)').where('post_id=? AND user_id=? AND like > 0', postId, userId) }
-  scope :disliked?, -> (postId, userId) { select('count(*)').where('post_id=? AND user_id=? AND dislike > 0', postId, userId) }
+  scope :num_of_likes, -> (postId) { select('count(*)').where('post_id=? AND "like" = true', postId) }
+  scope :num_of_dislikes, -> (postId) { select('count(*)').where('post_id=? AND "dislike" = true', postId) }
+  scope :liked?, -> (postId, userId) { select('count(*)').where('post_id=? AND user_id=? AND "like" = true', postId, userId) }
+  scope :disliked?, -> (postId, userId) { select('count(*)').where('post_id=? AND user_id=? AND dislike = true', postId, userId) }
 end
