4c93a5ac8748be935cf52e0e59322b4aae47c431
Ruby Chi <c90206@gmail.com>
2023-09-19 22:40:49 -0700

    Migrating PostgreSQL from Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_09_15_081421) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
